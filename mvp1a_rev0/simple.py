646bd4cae71fceab9608dfd8104a362afee22435
Sae Woo Nam <nams@nist.gov>
2020-05-17 10:43:49 -0600

    tweak readme tweak simple.py so that you can add a filename as the last parameter

--- mvp1a_rev0/simple.py
+++ mvp1a_rev0/simple.py
@@ -1,10 +1,16 @@
 import serial
 import glob
 import time
+import sys
+
 new_dongle_port = glob.glob('/dev/tty.usbmodem*')
 dongle_port = new_dongle_port[0]
 
-f = open('encounters.dat', 'wb')
+if len(sys.argv)> 1:
+    fname = sys.argv[1]
+else:
+    fname = 'encounters.dat'
+f = open(fname, 'wb')
 s = serial.Serial(dongle_port)
 # write file headers
 s.write(b'u')
@@ -12,6 +18,8 @@ response = s.readline()
 f.write(b"# "+response);
 print('readback time: ', response)
 msg = b"#  dongle_time, rssi, mac, adv_packet"
+padding = 99-len(msg);
+msg = msg+b" "*padding + b"\n"
 f.write(msg)
 
 s.write(b'g')
